6265e89b09dfa6be3b4cbb47060385c8bac9b264
Tom Schrogl <tom-schrogl@web.de>
2020-10-23 15:15:11 +0200

    Basic foundation includes:
    - simple gui timer running down
    - start and stop button
    - a combobox filled with minutes to choose

---
+++ ShutdownTimer/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace ShutdownTimer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbl_minutes = new System.Windows.Forms.Label();
+            this.cb_minutes = new System.Windows.Forms.ComboBox();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.btn_stop = new System.Windows.Forms.Button();
+            this.lbl_timer = new System.Windows.Forms.Label();
+            this.t_timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lbl_minutes
+            // 
+            this.lbl_minutes.AutoSize = true;
+            this.lbl_minutes.Location = new System.Drawing.Point(102, 55);
+            this.lbl_minutes.Name = "lbl_minutes";
+            this.lbl_minutes.Size = new System.Drawing.Size(44, 13);
+            this.lbl_minutes.TabIndex = 0;
+            this.lbl_minutes.Text = "Minutes";
+            // 
+            // cb_minutes
+            // 
+            this.cb_minutes.FormattingEnabled = true;
+            this.cb_minutes.Location = new System.Drawing.Point(64, 71);
+            this.cb_minutes.Name = "cb_minutes";
+            this.cb_minutes.Size = new System.Drawing.Size(121, 21);
+            this.cb_minutes.TabIndex = 1;
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(12, 251);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(103, 23);
+            this.btn_start.TabIndex = 2;
+            this.btn_start.Text = "Start";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // btn_stop
+            // 
+            this.btn_stop.Location = new System.Drawing.Point(121, 251);
+            this.btn_stop.Name = "btn_stop";
+            this.btn_stop.Size = new System.Drawing.Size(105, 23);
+            this.btn_stop.TabIndex = 3;
+            this.btn_stop.Text = "Stop";
+            this.btn_stop.UseVisualStyleBackColor = true;
+            this.btn_stop.Click += new System.EventHandler(this.btn_stop_Click);
+            // 
+            // lbl_timer
+            // 
+            this.lbl_timer.AutoSize = true;
+            this.lbl_timer.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_timer.Location = new System.Drawing.Point(22, 115);
+            this.lbl_timer.Name = "lbl_timer";
+            this.lbl_timer.Size = new System.Drawing.Size(194, 73);
+            this.lbl_timer.TabIndex = 4;
+            this.lbl_timer.Text = "00:00";
+            // 
+            // t_timer
+            // 
+            this.t_timer.Interval = 1000;
+            this.t_timer.Tick += new System.EventHandler(this.t_timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(254, 300);
+            this.Controls.Add(this.lbl_timer);
+            this.Controls.Add(this.btn_stop);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.cb_minutes);
+            this.Controls.Add(this.lbl_minutes);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_minutes;
+        private System.Windows.Forms.ComboBox cb_minutes;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Button btn_stop;
+        private System.Windows.Forms.Label lbl_timer;
+        private System.Windows.Forms.Timer t_timer;
+    }
+}
+
---
+++ ShutdownTimer/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShutdownTimer
+{
+    public partial class Form1 : Form
+    {
+        private int totalSeconds;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 30; i <= 180; i += 30)
+            {
+                this.cb_minutes.Items.Add(i.ToString());
+            }
+            this.cb_minutes.SelectedItem = "30";
+            this.btn_stop.Enabled = false;
+        }
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            this.btn_start.Enabled = false;
+            this.btn_stop.Enabled = true;
+
+            totalSeconds = int.Parse(this.cb_minutes.SelectedItem.ToString()) * 60;
+
+            System.Diagnostics.Process.Start("shutdown", "/s /t " + totalSeconds);
+
+            this.t_timer.Enabled = true;
+        }
+
+        private void btn_stop_Click(object sender, EventArgs e)
+        {
+            this.btn_stop.Enabled = false;
+            this.btn_start.Enabled = true;
+
+            System.Diagnostics.Process.Start("shutdown", "/a");
+        }
+
+        private void t_timer_Tick(object sender, EventArgs e)
+        {
+            if (totalSeconds > 0 && btn_stop.Enabled)
+            {
+                totalSeconds--;
+                int minutes = totalSeconds / 60;
+                int seconds = totalSeconds - (minutes * 60);
+                this.lbl_timer.Text = minutes.ToString() + ":" + seconds.ToString();
+            }
+            else
+            {
+                this.t_timer.Stop();
+                MessageBox.Show("Time's up!");
+            }
+        }
+    }
+}
